1b0f3d8a0333a663b527e1c6ed00f9958b8e623e
VuKieuAnh <vukieuanh.hnue@gmail.com>
2024-01-04 14:09:48 +0700

    add model Fiction Book

---
+++ src/model/FictionBook.java
@@ -0,0 +1,32 @@
+package model;
+
+public class FictionBook extends Book {
+    private String category;
+
+    public FictionBook() {
+    }
+
+    public FictionBook(String category) {
+        this.category = category;
+    }
+
+    public FictionBook(String code, String name, int price, String author, String category) {
+        super(code, name, price, author);
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "FictionBook{" +
+                "category='" + category + '\'' +
+                '}';
+    }
+}
