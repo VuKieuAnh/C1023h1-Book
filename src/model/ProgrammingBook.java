823be085ea44ac18de2023843835f0ac40a56c20
VuKieuAnh <vukieuanh.hnue@gmail.com>
2024-01-04 14:12:01 +0700

    add model Programming Book

---
+++ src/model/ProgrammingBook.java
@@ -0,0 +1,38 @@
+package model;
+
+public class ProgrammingBook extends Book {
+    private String language;
+    private String framework;
+
+    public ProgrammingBook(String language) {
+        this.language = language;
+    }
+
+    public ProgrammingBook() {
+    }
+
+    public ProgrammingBook(String code, String name, int price, String author) {
+        super(code, name, price, author);
+    }
+
+    public ProgrammingBook(String code, String name, int price, String author, String language) {
+        super(code, name, price, author);
+        this.language = language;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getFramework() {
+        return framework;
+    }
+
+    public void setFramework(String framework) {
+        this.framework = framework;
+    }
+}
